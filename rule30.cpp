573fc9ba55642d7be7dd7ffc6a8068844b914f21
Justin Meimar <j.meimar111@gmail.com>
2021-12-17 10:45:06 -0700

    rule30

---
+++ rule30.cpp
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <iostream>
+
+int main() {
+  uint64_t state = 1u << 31;
+
+  for (int i=0 ; i<32 ; ++i) {
+    for (int j=64 ; j-- ;) {
+      std::cout << char(state >> j & 1 ? '1' : '-');
+    }
+    std::cout << '\n';
+    state = (state >> 1) ^ (state | state << 1);
+  }
+  return 0;
+}
